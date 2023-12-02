fca9e2e889538a54cd44bf03218760c57fbff812
Valerio Romano Cadura <103530684+paranoidvaro@users.noreply.github.com>
2023-12-02 15:46:29 +0100

    Problem Set 3

---
+++ k_means/k_means.py
@@ -0,0 +1,230 @@
+from __future__ import division, print_function
+import argparse
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import random
+
+
+def init_centroids(num_clusters, image):
+    """
+    Initialize a `num_clusters` x image_shape[-1] nparray to RGB
+    values of randomly chosen pixels of`image`
+
+    Parameters
+    ----------
+    num_clusters : int
+        Number of centroids/clusters
+    image : nparray
+        (H, W, C) image represented as an nparray
+
+    Returns
+    -------
+    centroids_init : nparray
+        Randomly initialized centroids
+    """
+
+    # *** START YOUR CODE ***
+    # raise NotImplementedError('init_centroids function not implemented')
+    # print(image)
+    pixels = image.reshape(-1, image.shape[-1])
+    
+    random_pixels = np.random.choice(pixels.shape[0], num_clusters, replace = False)
+
+    centroids_init = pixels[random_pixels]
+
+
+    # *** END YOUR CODE ***
+
+    return centroids_init
+
+
+def update_centroids(centroids, image, max_iter=30, print_every=10):
+    """
+    Carry out k-means centroid update step `max_iter` times
+
+    Parameters
+    ----------
+    centroids : nparray
+        The centroids stored as an nparray
+    image : nparray
+        (H, W, C) image represented as an nparray
+    max_iter : int
+        Number of iterations to run
+    print_every : int
+        Frequency of status update
+
+    Returns
+    -------
+    new_centroids : nparray
+        Updated centroids
+    """
+
+    # *** START YOUR CODE ***
+    # raise NotImplementedError('update_centroids function not implemented')
+        # Usually expected to converge long before `max_iter` iterations
+                # Initialize `dist` vector to keep track of distance to every centroid
+                # Loop over all centroids and store distances in `dist`
+                # Find closest centroid and update `new_centroids`
+        # Update `new_centroids`
+    # *** END YOUR CODE ***
+    
+    pixels = image.reshape(-1, image.shape[-1])
+    
+
+    for iteration in range(max_iter):
+        
+        new_centroids = np.zeros_like(centroids, dtype=np.float64)
+        
+        counts = np.zeros(len(centroids), dtype=int)
+
+        for pixel in pixels:
+            
+            closest = np.argmin(np.linalg.norm(centroids - pixel, axis=1))
+            
+            new_centroids[closest] += pixel
+            
+            counts[closest] += 1
+
+        
+        for i in range(len(centroids)):
+            if counts[i] > 0:  
+                new_centroids[i] /= counts[i]
+
+        
+        if np.all(centroids == new_centroids):
+            print(f"Convergence reached after {iteration} iterations.")
+            break
+
+        
+        centroids = new_centroids
+
+        
+        if iteration % print_every == 0:
+            print(f"Iteration {iteration}")
+
+    return centroids
+
+def update_image(image, centroids):
+    """
+    Update RGB values of pixels in `image` by finding
+    the closest among the `centroids`
+
+    Parameters
+    ----------
+    image : nparray
+        (H, W, C) image represented as an nparray
+    centroids : int
+        The centroids stored as an nparray
+
+    Returns
+    -------
+    image : nparray
+        Updated image
+    """
+
+    # *** START YOUR CODE ***
+    # raise NotImplementedError('update_image function not implemented')
+            # Initialize `dist` vector to keep track of distance to every centroid
+            # Loop over all centroids and store distances in `dist`
+            # Find closest centroid and update pixel value in `image`
+    
+    original_shape = image.shape
+    image = image.reshape(-1, original_shape[-1])
+ 
+    image_extended = image[:, None]
+
+    differences = image_extended - centroids
+
+    distances = np.linalg.norm(differences, axis = 2)
+
+    closest_centroids = np.argmin(distances, axis = 1)
+
+    image = centroids[closest_centroids].astype(image.dtype)
+
+    image = image.reshape(original_shape)
+
+    return image
+
+
+
+
+
+
+
+
+def main(args):
+
+    # Setup
+    max_iter = args.max_iter
+    print_every = args.print_every
+    image_path_small = args.small_path
+    image_path_large = args.large_path
+    num_clusters = args.num_clusters
+    figure_idx = 0
+
+    # Load small image
+    image = np.copy(mpimg.imread(image_path_small))
+    print('[INFO] Loaded small image with shape: {}'.format(np.shape(image)))
+    plt.figure(figure_idx)
+    figure_idx += 1
+    plt.imshow(image)
+    plt.title('Original small image')
+    plt.axis('off')
+    savepath = os.path.join('.', 'orig_small.png')
+    plt.savefig(savepath, transparent=True, format='png', bbox_inches='tight')
+
+    # Initialize centroids
+    print('[INFO] Centroids initialized')
+    centroids_init = init_centroids(num_clusters, image)
+
+    # Update centroids
+    print(25 * '=')
+    print('Updating centroids ...')
+    print(25 * '=')
+    centroids = update_centroids(centroids_init, image, max_iter, print_every)
+
+    # # Load large image
+    image = np.copy(mpimg.imread(image_path_large))
+    image.setflags(write=1)
+    print('[INFO] Loaded large image with shape: {}'.format(np.shape(image)))
+    plt.figure(figure_idx)
+    figure_idx += 1
+    plt.imshow(image)
+    plt.title('Original large image')
+    plt.axis('off')
+    savepath = os.path.join('.', 'orig_large.png')
+    plt.savefig(fname=savepath, transparent=True, format='png', bbox_inches='tight')
+
+    # # Update large image with centroids calculated on small image
+    print(25 * '=')
+    print('Updating large image ...')
+    print(25 * '=')
+    image_clustered = update_image(image, centroids)
+
+    plt.figure(figure_idx)
+    figure_idx += 1
+    plt.imshow(image_clustered)
+    plt.title('Updated large image')
+    plt.axis('off')
+    savepath = os.path.join('.', 'updated_large.png')
+    plt.savefig(fname=savepath, transparent=True, format='png', bbox_inches='tight')
+
+    print('\nCOMPLETE')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--small_path', default='./peppers-small.tiff',
+                        help='Path to small image')
+    parser.add_argument('--large_path', default='./peppers-large.tiff',
+                        help='Path to large image')
+    parser.add_argument('--max_iter', type=int, default=150,
+                        help='Maximum number of iterations')
+    parser.add_argument('--num_clusters', type=int, default=16,
+                        help='Number of centroids/clusters')
+    parser.add_argument('--print_every', type=int, default=10,
+                        help='Iteration print frequency')
+    args = parser.parse_args()
+    main(args)
